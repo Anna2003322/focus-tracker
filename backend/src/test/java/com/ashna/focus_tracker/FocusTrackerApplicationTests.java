8e8de7eadc614d4402f2e25ff25995dd4e9d5422
Anna2003322 <annaashna21@gmail.com>
2026-08-22 12:24:16 +0530

    feat: scaffold Spring Boot backend with dev/prod profiles

---
+++ backend/src/test/java/com/ashna/focus_tracker/FocusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashna.focus_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
